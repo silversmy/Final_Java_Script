093adb7bb27ff78d243aaac3ede58667002dc336
Izunegbu Daniel <chukwuma234daniel@gmail.com>
2026-09-16 13:48:22 +0100

    Use environment secret for JWT

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const env = require("./env");
 
 const aToken = (payload) => {
-   return jwt.sign(payload, "jwt-secret", { expiresIn: "1h" });
+  return jwt.sign(payload, env.aSecret, { expiresIn: "1h" });
 };
 
-module.exports =aToken;
+module.exports = aToken;
